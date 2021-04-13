2e5e2adae0a9ab011ff75f7ca1e6d21386906cfe
emilykidd3 <e.kidd61@yahoo.com>
2021-04-13 16:11:27 -0700

    added optimization for photos

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,52 @@
 // gives Cannot read property 'jquery' of undefined error when added in
 // require("bootstrap");
-const webpack = require("webpack")
+const webpack = require("webpack");
 const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-  entry: "./assets/js/script.js",
+  entry: {
+      app: "./assets/js/script.js",
+      events: "./assets/js/events.js",
+      schedule: "./assets/js/schedule.js",
+      tickets: "./assets/js/tickets.js"
+    },
   output: {
-    path: path.join(__dirname + "/dist"),
-    filename: "main.bundle.js"
+    path: __dirname + "/dist",
+    filename: "[name].bundle.js",
+  },
+  module: {
+    rules: [
+        {
+            test: /\.jpg$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name (file) {
+                            return "[path][name].[ext]"
+                        },
+                        publicPath: function(url) {
+                            return url.replace('../', '/assets/')
+                        }
+                    }
+                },
+                {
+                    loader: 'image-webpack-loader'
+                }
+            ]
+        }
+    ]
   },
   plugins: [
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new BundleAnalyzerPlugin({
+        analyzerMode: "static", // the report outputs to a HTML file in the dist folder
+    })
   ],
-  mode: "development"
+  mode: "development",
 };
